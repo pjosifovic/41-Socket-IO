98f57d2bb989ccf9bb8ec726d54fada75b5cf895
Predrag Josifovic <pjosifovic@yahoo.com>
2018-01-29 19:04:31 -0800

    server up and running

--- lab-pedja/index.js
+++ lab-pedja/index.js
@@ -1,3 +1,17 @@
 'use strict';
 
-console.log("working");
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+require('dotenv').config();
+
+app.use(express.static('./public'));
+
+//TODO: add io listener io.on
+
+http.listen(process.env.PORT, () => {
+  console.log(`Server started on port: ${process.env.PORT}`);
+});
+
