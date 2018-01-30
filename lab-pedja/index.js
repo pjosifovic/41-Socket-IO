80a6af99e2e89b1f5c41becf2ef00733fcfa8ac2
Predrag Josifovic <pjosifovic@yahoo.com>
2018-01-29 19:19:00 -0800

    displaying chat messages

--- lab-pedja/index.js
+++ lab-pedja/index.js
@@ -10,6 +10,19 @@ require('dotenv').config();
 app.use(express.static('./public'));
 
 //TODO: add io listener io.on
+io.on('connection', socket => {
+  console.log('JOINED CHAT', socket.id);
+ 
+  socket.on('disconnect', () => {
+    console.log('LEFT CHAT', socket.id);
+  })
+
+  socket.on('send-message', data => {
+    console.log('DATA MESSAGE', data.message);
+    io.emit('receive-message', data);
+  });
+  
+})
 
 http.listen(process.env.PORT, () => {
   console.log(`Server started on port: ${process.env.PORT}`);
--- lab-pedja/public/js/main.js
+++ lab-pedja/public/js/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-console.log('JS working');
+const socket = io();
+console.log('socket_ID:', socket.id);
+
+let sendMessageForm = document.getElementById('send-message-form')
+let messageInput = document.getElementById('message-input')
+let messageContainer = document.getElementById('messages')
+
+sendMessageForm.addEventListener('submit', event => {
+  event.preventDefault();
+  let message = messageInput.value;
+  socket.emit('send-message', { message });
+});
+
+socket.on('receive-message', data => {
+  console.log('RECEIVED', data);
+  let div = document.createElement('div')
+  div.textContent = data.message;
+  messageContainer.appendChild(div);
+})
+
